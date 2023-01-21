da40a36cc8ef69f173310eb7afabd4a1e5cdc9d7
kizunas <8s6s7e@gmail.com>
2023-01-21 16:25:51 +0900

    vuexとv-modelを組み合わせたい1（v-modelを使わないパターン）

--- src/views/HomeViews.vue
+++ src/views/HomeViews.vue
@@ -4,6 +4,8 @@
     <button @click="toUsers">Usersのページに行く</button>
     <p>{{ doubleCount }}</p>
     <p>{{ tripleCount }}</p>
+    <input type="text" :value="message" @input="updateMssage">
+    <p>{{ message }}</p>
   </div>
 </template>
 
@@ -13,8 +15,15 @@ import { mapGetters } from 'vuex';
 export default {
   computed: {
     ...mapGetters(["doubleCount", "tripleCount"]),
+    message() {
+      return this.$store.getters.message;
+    }
   },
   methods: {
+    updateMssage(e) { // イベントを取得し、actionへ値を送る。
+      this.$store.dispatch("updateMessage", e.target.value)
+      console.log(e);
+    },
     toUsers() {
       this.$router.push({
         name: "users-id-profile",
