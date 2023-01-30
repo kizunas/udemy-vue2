a61bbc76cc555c4ed560ce204fbdabae60a8fe35
kizunas <8s6s7e@gmail.com>
2023-01-30 16:02:55 +0900

    route-linkタグ

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app">
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <router-link to="/">Home</router-link> <!-- aタグのようにサーバーにアクセスされないため切り替えが早い -->
+      <router-link to="/users">Users</router-link>
+    </nav>
     <router-view></router-view>  <!-- URLが変わるとそれに付随したコンポーネントを表示させるために必要。 -->
   </div>
 </template>
