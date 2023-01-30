27c52d367a2696f34db736547a001f512c877b9d
kizunas <8s6s7e@gmail.com>
2023-01-30 16:16:47 +0900

    active-class属性とexact属性

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
     <nav>
-      <router-link to="/">Home</router-link> <!-- aタグのようにサーバーにアクセスされないため切り替えが早い -->
-      <router-link to="/users">Users</router-link>
+      <router-link 
+       to="/"
+       active-class="link--active"
+       exact
+       class="link"
+       >Home</router-link> <!-- aタグのようにサーバーにアクセスされないため切り替えが早い -->
+      <router-link 
+       to="/users"
+       active-class="link--active"
+       exact
+       class="link">Users</router-link>
     </nav>
     <router-view></router-view>  <!-- URLが変わるとそれに付随したコンポーネントを表示させるために必要。 -->
   </div>
 </template>
+
+<style scoped>
+.link {
+  margin-right: 10px;
+}
+.link--active {
+  font-size: 20px;
+}
+</style>
