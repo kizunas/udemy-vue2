5dd5c6281e6ed91db55b8ec6fef907af91d2858e
kizunas <8s6s7e@gmail.com>
2023-02-04 16:56:36 +0900

    ライフサイクルフックが呼ばれない

--- src/views/UsersViews.vue
+++ src/views/UsersViews.vue
@@ -2,6 +2,16 @@
   <div>
     <h3>Users</h3>
     <hr> <!-- <hr> 水平線を書くタグ -->
+    <router-link to="/users/1">ユーザー１</router-link>
+    <router-link to="/users/a">ユーザーa</router-link>
     <h1>User No. {{ $route.params.id }}</h1> <!-- router.js内で今回users/:idとしたので取得するparamsはidとしている。 -->
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    console.log("createdされました");
+  }
+}
+</script>
