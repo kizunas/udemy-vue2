401e0e1d3c0257612e05599ff5b0662182429ec1
kizunas <8s6s7e@gmail.com>
2023-02-04 17:37:34 +0900

    propsをtrueにし、コンポーネント利用性を高める

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ export default new Router({
   // このURLではこのコンポーネントを出すという設定をする。↴
   routes: [
     { path: '/', component: Home }, 
-    { path: '/users/:id', component: Users } // コロン（:）をつけると動的にできる。基本的には:idと書く
+    { path: '/users/:id', component: Users, props: true } // コロン（:）をつけると動的にできる。基本的には:idと書く。
+    // props: trueとするとpropsが使える。users/:idのidが返されるようになる。すなわちusers/1なら1を渡す。
   ]
 })
--- src/views/UsersViews.vue
+++ src/views/UsersViews.vue
@@ -4,18 +4,12 @@
     <hr> <!-- <hr> 水平線を書くタグ -->
     <router-link to="/users/1">ユーザー１</router-link>
     <router-link to="/users/a">ユーザーa</router-link>
-    <h1>User No. {{ $route.params.id }}</h1> <!-- router.js内で今回users/:idとしたので取得するparamsはidとしている。 -->
+    <h1>User No. {{ id }}</h1> <!-- propsによりrouter.jsのusers/:idのidが渡されるようになった。 -->
   </div>
 </template>
 
 <script>
 export default {
-  watch: {
-    $route(to, from) { // to, fromの名前は何でもよい。第一引数が新しい値、第二引数が古い値を返してくれる。
-      console.log(to);
-      console.log(from);
-      console.log("$routerが変わった" + from.params.id + "から" + to.params.id);
-    }
-  }
+  props: ["id"] // 親コンポーネントから渡された物ではないため、再利用する際は親コンポーネントから別のidというものを渡してあげると上のマスタッシュ構文のidで表示されるものが違うものにできる。
 }
 </script>
